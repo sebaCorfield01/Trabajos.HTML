2788bcb03f05d0861ddeba46ebaa78e3afa4e2db
sebaCorfield01 <sebacorf@hotmail.com>
2024-05-17 16:32:18 -0300

    Create 2.js

---
+++ java.s/2.js
@@ -0,0 +1,20 @@
+function cuenta(num1,num2){
+    resultado = num1 + num2;
+    if(resultado < 0 ){
+        resultado = resultado * -1
+    }
+    console.log(resultado);
+
+
+}
+
+function main(){
+   
+   let = num1 = parseInt(prompt("ingrese el primer numero "));
+   let = num2 = parseInt(prompt("ingrese el segundo numero "));
+
+   cuenta(num1,num2);
+
+}
+
+main();
